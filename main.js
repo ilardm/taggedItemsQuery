f8686883b08b5efa88a5119241938dfb1581b8b5
Ilya Arefiev <arefiev.id@gmail.com>
2014-05-12 14:44:14 +0400

    Fetch Artist/Track/Tag info

--- main.js
+++ main.js
@@ -30,56 +30,120 @@ $(document).ready(function() {
         cache     : cache
     });
 
-    throttle( function() {
-        lastfm.artist.getInfo( {"artist": "Electric Light Orchestra"},
-                               {  "success": onApiResponse,
-                                  "error": onApiError
-                               });
-    }, apiCallInterval );
+    var DATACACHE = { artists: [],
+                      tracks: [],
+                      tags: []
+                    };
 
-    throttle( function() {
-        lastfm.artist.getTopTags( {"artist": "Electric Light Orchestra"},
-                                  {  "success": onApiResponse,
-                                     "error": onApiError
-                                  });
-    }, apiCallInterval );
+    var lastfmUser = "aid9990";
 
-    throttle( function() {
-        lastfm.track.getInfo( {"artist": "Electric Light Orchestra",
-                               "track": "Do Ya"
-                              },
-                              { "success": onApiResponse,
-                                "error": onApiError
-                              }
-                            );
-    }, apiCallInterval );
+    // ---------------------------------------------------------------
 
-    throttle( function() {
-        lastfm.track.getTopTags( {"artist": "Electric Light Orchestra",
-                                  "track": "Do Ya"
-                                 },
-                                 {  "success": onApiResponse,
-                                    "error": onApiError
-                                 });
-    }, apiCallInterval );
+    var processTopTags = function(data, getter, setter) {
+        var tags = getter("tags");
 
-    throttle( function() {
-        lastfm.user.getTopArtists( {"user": "aid9990",
+        for ( var i = 0; i < data.toptags.tag.length; ++i ) {
+            (function(tag) {
+                if ( tags.indexOf( tag.name ) == -1 ) {
+                    tags.push( tag.name );
+                }
+
+                if ( DATACACHE.tags.indexOf( tag.name ) == -1 ) {
+                    DATACACHE.tags.push( tag.name );
+                }
+            })(data.toptags.tag[i]);
+        }
+
+        setter("tags", tags);
+    }
+
+    var processTopTagsOfArtist = function(data, artist) {
+        processTopTags(data, function(key) {
+            return DATACACHE.artists[artist][key];
+        }, function(key, value) {
+            DATACACHE.artists[artist][key] = value;
+        });
+    }
+
+    var processTopTagsOfTrack = function(data, track) {
+        processTopTags(data, function(key) {
+            return DATACACHE.tracks[track][key];
+        }, function(key, value) {
+            DATACACHE.tracks[track][key] = value;
+        });
+    }
+
+    var requestArtistTags = function(artistName) {
+        if ( DATACACHE.artists[artistName] === undefined ) {
+            DATACACHE.artists[artistName] = { name: artistName,
+                                              tags: []
+                                            };
+
+            throttle(function(){
+                lastfm.artist.getTopTags( { "artist": artistName},
+                                          { "success": function(data) {
+                                              processTopTagsOfArtist(data, artistName);
+                                          },
+                                            "error": onApiError
+                                          });
+            }, apiCallInterval);
+        }
+    }
+
+    var processTopArtists = function(data) {
+        for ( var i = 0; i < data.topartists.artist.length; ++i ) {
+            (function(artist) {
+                requestArtistTags(artist.name);
+            })(data.topartists.artist[i]);
+        }
+    }
+
+    var processTopTracks = function(data) {
+        for ( var i = 0; i < data.toptracks.track.length; ++i ) {
+            (function(track) {
+                if ( DATACACHE.tracks[track.name] === undefined ) {
+                    DATACACHE.tracks[track.name] = { name: track.name,
+                                                     artist: track.artist.name,
+                                                     tags: []
+                                                   };
+                    if ( DATACACHE.artists[track.artist.name] === undefined ) {
+                        requestArtistTags(track.artist.name);
+                    }
+
+                    throttle(function(){
+                        lastfm.track.getTopTags( { "artist": track.artist.name,
+                                                   "track": track.name
+                                                 },
+                                                 { "success": function(data) {
+                                                     processTopTagsOfTrack(data, track.name);
+                                                 },
+                                                   "error": onApiError
+                                                 });
+                    }, apiCallInterval);
+                }
+            })(data.toptracks.track[i]);
+        }
+    }
+
+    // ---------------------------------------------------------------
+
+    throttle(function(){
+        lastfm.user.getTopArtists( {"user": lastfmUser,
                                     "period": "3month",
                                    },
-                                   { "success": onApiResponse,
+                                   { "success": processTopArtists,
                                      "error": onApiError
                                    }
                                  );
-    }, apiCallInterval );
+    }, apiCallInterval);
 
-    throttle( function() {
-        lastfm.user.getTopTracks( {"user": "aid9990",
+    throttle(function(){
+        lastfm.user.getTopTracks( {"user": lastfmUser,
                                    "period": "3month",
                                   },
-                                  { "success": onApiResponse,
+                                  { "success": processTopTracks,
                                     "error": onApiError
                                   }
                                 );
-    }, apiCallInterval );
+    }, apiCallInterval);
 });
